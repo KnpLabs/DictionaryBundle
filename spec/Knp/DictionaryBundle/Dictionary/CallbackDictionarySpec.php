c0ef94fb894d94fca2644136c1ab541e63e85e13
Antoine Lelaisant <antoine.lelaisant@knplabs.com>
2016-11-25 23:08:42 +0100

    Introduce CallbackDictionary

--- spec/Knp/DictionaryBundle/Dictionary/CallbackDictionarySpec.php
+++ spec/Knp/DictionaryBundle/Dictionary/CallbackDictionarySpec.php
@@ -4,7 +4,7 @@ namespace spec\Knp\DictionaryBundle\Dictionary;
 
 use PhpSpec\ObjectBehavior;
 
-class CallableDictionarySpec extends ObjectBehavior
+class CallbackDictionarySpec extends ObjectBehavior
 {
     /**
      * @var bool
@@ -24,7 +24,7 @@ class CallableDictionarySpec extends ObjectBehavior
 
     function it_is_initializable()
     {
-        $this->shouldHaveType('Knp\DictionaryBundle\Dictionary\CallableDictionary');
+        $this->shouldHaveType('Knp\DictionaryBundle\Dictionary\CallbackDictionary');
     }
 
     function it_is_a_dictionary()
---
+++ spec/Knp/DictionaryBundle/Dictionary/Factory/CallbackFactorySpec.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace spec\Knp\DictionaryBundle\Dictionary\Factory;
+
+use PhpSpec\ObjectBehavior;
+use Symfony\Component\DependencyInjection\Container;
+
+class CallbackFactorySpec extends ObjectBehavior
+{
+    function let(Container $container)
+    {
+        $this->beConstructedWith($container);
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType('Knp\DictionaryBundle\Dictionary\Factory\CallbackFactory');
+    }
+
+    function it_is_a_factory()
+    {
+        $this->shouldHaveType('Knp\DictionaryBundle\Dictionary\Factory');
+    }
+
+    function it_supports_specific_config()
+    {
+        $this->supports(array('type' => 'callback'))->shouldReturn(true);
+    }
+
+    function it_creates_a_dictionary(
+        $container,
+        MockedService $service
+    ) {
+        $config = array(
+            'service' => 'service.id',
+            'method'  => 'getYolo',
+        );
+
+        $container->get('service.id')->willReturn($service);
+        $service->getYolo()->willReturn(array(
+            'foo1' => 'bar1',
+            'foo2' => 'bar2',
+            'foo3' => 'bar3',
+        ));
+
+        $dictionary = $this->create('yolo', $config);
+
+        $dictionary->getName()->shouldBe('yolo');
+        $dictionary->getValues()->shouldBe(array(
+            'foo1' => 'bar1',
+            'foo2' => 'bar2',
+            'foo3' => 'bar3',
+        ));
+    }
+}
+
+class MockedService
+{
+    public function getYolo()
+    {
+    }
+}
