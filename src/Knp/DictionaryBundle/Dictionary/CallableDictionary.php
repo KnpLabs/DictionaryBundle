9f82bba7d0085068ef8f63aebc0b8164831b8bad
Albin Kerouanton <albin.kerouanton@knplabs.com>
2017-04-02 14:31:22 +0200

    Supports arguments for the callable used to hydrate CallableDictionary

--- src/Knp/DictionaryBundle/Dictionary/CallableDictionary.php
+++ src/Knp/DictionaryBundle/Dictionary/CallableDictionary.php
@@ -21,14 +21,19 @@ class CallableDictionary implements Dictionary
      */
     private $callable;
 
+    /** @var array */
+    private $callableArgs;
+
     /**
      * @param string   $name
      * @param callable $callable
+     * @param array    $callableArgs
      */
-    public function __construct($name, callable $callable)
+    public function __construct($name, callable $callable, array $callableArgs = [])
     {
-        $this->name     = $name;
-        $this->callable = $callable;
+        $this->name         = $name;
+        $this->callable     = $callable;
+        $this->callableArgs = $callableArgs;
     }
 
     /**
@@ -120,7 +125,7 @@ class CallableDictionary implements Dictionary
             return;
         }
 
-        $values = call_user_func($this->callable);
+        $values = call_user_func_array($this->callable, $this->callableArgs);
 
         if (false === is_array($values) && false === $values instanceof \ArrayAccess) {
             throw new \InvalidArgumentException(
