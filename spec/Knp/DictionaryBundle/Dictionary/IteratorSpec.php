2ba8903516d086c4456fe748be6eeec099ce10e3
Pierre PLAZANET <plazanet.pierre@gmail.com>
2019-03-06 18:28:12 +0100

    Add iterator based dictionary

---
+++ spec/Knp/DictionaryBundle/Dictionary/IteratorSpec.php
@@ -0,0 +1,118 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Knp\DictionaryBundle\Dictionary;
+
+use Exception;
+use Generator;
+use Knp\DictionaryBundle\Dictionary;
+use PhpSpec\ObjectBehavior;
+
+class IteratorSpec extends ObjectBehavior
+{
+    /**
+     * @var bool
+     */
+    private $executed;
+
+    function let()
+    {
+        $this->executed = false;
+
+        $this->beConstructedWith('foo', $this->execution());
+    }
+
+    function it_is_initializable()
+    {
+        $this->shouldHaveType(Dictionary\Iterator::class);
+    }
+
+    function it_is_a_dictionary()
+    {
+        $this->shouldImplement(Dictionary::class);
+    }
+
+    function it_supports_some_array_access_functions()
+    {
+        $dictionary = $this->getWrappedObject();
+
+        $this['foo']->shouldBe(0);
+        $this->offsetExists('foo')->shouldReturn(true);
+
+        $this['foo'] = 'test';
+        $this['foo']->shouldBe('test');
+
+        unset($dictionary['foo']);
+        $this->offsetExists('foo')->shouldReturn(false);
+    }
+
+    function it_provides_a_set_of_values()
+    {
+        $this->getValues()->shouldReturn([
+            'foo' => 0,
+            'bar' => 1,
+            'baz' => 2,
+        ]);
+    }
+
+    function it_provides_a_set_of_keys()
+    {
+        $this->getKeys()->shouldReturn([
+            'foo',
+            'bar',
+            'baz',
+        ]);
+    }
+
+    function it_is_hydrated_just_once()
+    {
+        $this->getValues()->shouldReturn([
+            'foo' => 0,
+            'bar' => 1,
+            'baz' => 2,
+        ]);
+
+        $this->getValues()->shouldReturn([
+            'foo' => 0,
+            'bar' => 1,
+            'baz' => 2,
+        ]);
+    }
+
+    function it_access_to_value_like_an_array()
+    {
+        $this['foo']->shouldReturn(0);
+        $this['bar']->shouldReturn(1);
+        $this['baz']->shouldReturn(2);
+    }
+
+    function it_generates_an_iterator()
+    {
+        $this->shouldIterateLike([
+            'foo' => 0,
+            'bar' => 1,
+            'baz' => 2,
+        ]);
+    }
+
+    function its_getname_should_return_dictionary_name()
+    {
+        $this->getName()->shouldReturn('foo');
+    }
+
+    private function execution(): Generator
+    {
+        if (false === $this->executed) {
+            $this->executed = true;
+
+            yield 'foo' => 0;
+            yield 'bar' => 1;
+            yield 'baz' => 2;
+
+            return;
+        }
+
+        throw new Exception('Executed twice.');
+    }
+}
---
+++ src/Knp/DictionaryBundle/Dictionary/Factory/Iterator.php
@@ -0,0 +1,57 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Knp\DictionaryBundle\Dictionary\Factory;
+
+use InvalidArgumentException;
+use Knp\DictionaryBundle\Dictionary;
+use Psr\Container\ContainerInterface;
+use Traversable;
+
+final class Iterator implements Dictionary\Factory
+{
+    /**
+     * @var ContainerInterface
+     */
+    private $container;
+
+    public function __construct(ContainerInterface $container)
+    {
+        $this->container = $container;
+    }
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throw InvalidArgumentException if there is some problem with the config.
+     */
+    public function create(string $name, array $config): Dictionary
+    {
+        if (!isset($config['service'])) {
+            throw new InvalidArgumentException(sprintf(
+                'The "service" config key must be set for the dictionary named "%s".',
+                $name
+            ));
+        }
+
+        $service = $this->container->get($config['service']);
+
+        if (false === $service instanceof Traversable) {
+            throw new InvalidArgumentException(sprintf(
+                'You must provide a valid instance of Traversable for the dictionary named "%s".',
+                $name
+            ));
+        }
+
+        return new Dictionary\Iterator($name, $callable);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function supports(array $config): bool
+    {
+        return (isset($config['type'])) ? 'iterator' === $config['type'] : false;
+    }
+}
