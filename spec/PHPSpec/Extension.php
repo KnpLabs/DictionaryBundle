2939bc4504ae3927af465ae1f93bb102e57bfca4
Pierre PLAZANET <plazanet.pierre@gmail.com>
2019-12-09 14:15:21 +0100

    Add PHPSpec extension

---
+++ spec/PHPSpec/Extension.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\PHPSpec;
+
+use PhpSpec;
+
+final class Extension implements PhpSpec\Extension
+{
+    public function load(PhpSpec\ServiceContainer $container, array $params): void
+    {
+        $container->define(
+            OneOfMatcher::class,
+            function (): OneOfMatcher {
+                return new OneOfMatcher();
+            },
+            ['matchers']
+        );
+    }
+}
