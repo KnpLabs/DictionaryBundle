52132ef5b88d085cca47969f0d127276e3a559fd
Pierre PLAZANET <plazanet.pierre@gmail.com>
2021-01-29 12:16:01 +0100

    feat: enable Symfony DIC autoconfiguration for dictionaries

--- src/Knp/DictionaryBundle/DependencyInjection/KnpDictionaryExtension.php
+++ src/Knp/DictionaryBundle/DependencyInjection/KnpDictionaryExtension.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Knp\DictionaryBundle\DependencyInjection;
 
+use Knp\DictionaryBundle\DependencyInjection\Compiler\DictionaryFactoryBuildingPass;
+use Knp\DictionaryBundle\DependencyInjection\Compiler\DictionaryRegistrationPass;
+use Knp\DictionaryBundle\Dictionary;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
@@ -23,6 +26,16 @@ final class KnpDictionaryExtension extends Extension
                 'knp_dictionary.configuration',
                 $this->processConfiguration($configuration, $config)
             )
+        ;
+
+        $container
+            ->registerForAutoconfiguration(Dictionary::class)
+            ->addTag(DictionaryRegistrationPass::TAG_DICTIONARY)
+        ;
+
+        $container
+            ->registerForAutoconfiguration(Dictionary\Factory::class)
+            ->addTag(DictionaryFactoryBuildingPass::TAG_FACTORY)
         ;
 
         $loader = new YamlFileLoader(
