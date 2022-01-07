f4964245354ce63110b6801606762196fff8f996
PedroTroller <pierre.plazanet@knplabs.com>
2022-01-07 17:06:39 +0100

    chore: upgrade php-cs-fixer version

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->append([__FILE__, __DIR__.'/.symfony_checker'])
+;
+
+$config = new PhpCsFixer\Config();
+$config->setFinder($finder);
+$config->registerCustomFixers(new PedroTroller\CS\Fixer\Fixers());
+$config->setRiskyAllowed(true);
+$config->setUsingCache(true);
+$config->setRules(
+    PedroTroller\CS\Fixer\RuleSetFactory::create()
+        ->phpCsFixer(true)
+        ->php(7.3, true)
+        ->pedrotroller(true)
+        ->enable('align_multiline_comment')
+        ->enable('array_indentation')
+        ->enable('binary_operator_spaces', [
+            'operators' => [
+                '='  => 'align_single_space_minimal',
+                '=>' => 'align_single_space_minimal',
+            ],
+        ])
+        ->enable('global_namespace_import', ['import_classes' => true, 'import_constants' => false, 'import_functions' => false])
+        ->enable('final_class')
+        ->enable('ordered_imports')
+        ->enable('ordered_interfaces')
+        ->enable('phpdoc_line_span')
+        ->disable('method_chaining_indentation')
+        ->disable('no_break_comment')
+        ->disable('no_superfluous_phpdoc_tags')
+        ->getRules()
+);
+
+return $config;
