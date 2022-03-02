f81e20d48bffad3d2236bf40f43bb4dc3c593efb
Tac Tacelosky <tacman@gmail.com>
2022-03-02 11:04:14 -0600

    fix, deprecation

--- src/Knp/DictionaryBundle/Dictionary/Simple.php
+++ src/Knp/DictionaryBundle/Dictionary/Simple.php
@@ -50,7 +50,7 @@ final class Simple implements Dictionary
     }
 
     #[\ReturnTypeWillChange]
-    public function offsetGet(mixed $offset): mixed
+    public function offsetGet($offset): mixed
     {
         return $this->values[$offset];
     }
