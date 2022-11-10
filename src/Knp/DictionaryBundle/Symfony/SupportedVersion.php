f9490c551f3d532434cc219cb94d8556afeff917
PedroTroller <pierre.plazanet@knplabs.com>
2022-11-10 15:17:38 +0100

    chore: drop support of Symfony 6.1 & 6.2

--- src/Knp/DictionaryBundle/Symfony/SupportedVersion.php
+++ src/Knp/DictionaryBundle/Symfony/SupportedVersion.php
@@ -7,8 +7,6 @@ namespace Knp\DictionaryBundle\Symfony;
 final class SupportedVersion
 {
     public const v5_4 = '5.4';
-    public const v6_0 = '6.0';
-    public const v6_1 = '6.1';
     public const v6_2 = '6.2';
 
     private function __construct()
