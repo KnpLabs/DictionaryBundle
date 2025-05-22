c79da1e4af0d06a0686a7b782dcd8be2c44fd1e2
PedroTroller <pierre.plazanet@knplabs.com>
2025-05-22 17:56:48 +0200

    refactor: upgrade phpstan

--- src/Knp/DictionaryBundle/ValueTransformer/Constant.php
+++ src/Knp/DictionaryBundle/ValueTransformer/Constant.php
@@ -10,7 +10,7 @@ use ReflectionClass;
 
 final class Constant implements ValueTransformer
 {
-    private const PATTERN = '/^(?P<class>.*)::(?P<constant>.*)$/';
+    private const PATTERN = '/^(.*)::(.*)$/';
 
     public function supports(mixed $value): bool
     {
@@ -24,15 +24,11 @@ final class Constant implements ValueTransformer
             return false;
         }
 
-        if (!class_exists($matches['class']) && !interface_exists($matches['class'])) {
-            return false;
-        }
+        [$class, $constant] = $matches;
 
-        $constants = (new ReflectionClass($matches['class']))
-            ->getConstants()
-        ;
+        $constants = (new ReflectionClass($class))->getConstants();
 
-        return \array_key_exists($matches['constant'], $constants);
+        return \array_key_exists($constant, $constants);
     }
 
     public function transform(mixed $value)
@@ -41,21 +37,22 @@ final class Constant implements ValueTransformer
             throw new Exception("Unable to resolve constant {$value}.");
         }
 
-        return (new ReflectionClass($matches['class']))
-            ->getConstant($matches['constant'])
-        ;
+        [$class, $constant] = $matches;
+
+        return (new ReflectionClass($class))->getConstant($constant);
     }
 
     /**
-     * @return ?array{class: class-string, constant: string}
+     * @return ?array{class-string, string}
      */
     private function extract(string $value): ?array
     {
         if (preg_match(self::PATTERN, $value, $matches)) {
-            /**
-             * @var array{class: class-string, constant: string} $matches
-             */
-            return $matches;
+            [, $class, $constant] = $matches;
+
+            return class_exists($class) || interface_exists($class)
+                ? [$class, $constant]
+                : null;
         }
 
         return null;
